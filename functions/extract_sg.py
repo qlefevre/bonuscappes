d4e9081786b8a38a2563267c239f9555066cd2fc
Quentin Lefèvre <qlefevre@gmail.com>
2023-01-23 23:17:10 +0100

    Fix

--- functions/extract_sg.py
+++ functions/extract_sg.py
@@ -1,8 +1,11 @@
 from openpyxl import load_workbook
 from os import path
 from io import BytesIO
+from io import BytesIO
+from tempfile import NamedTemporaryFile
 import sys
 import urllib.request
+import base64
 
 # Charge un fichier Excel depuis url
 
@@ -10,6 +13,13 @@ import urllib.request
 def load_workbook_from_url(url):
     file = urllib.request.urlopen(url).read()
     return load_workbook(filename=BytesIO(file))
+
+
+def save_virtual_workbook(workbook):
+    with NamedTemporaryFile(delete=False) as tf:
+        workbook.save(tf.name)
+        in_memory = BytesIO(tf.read())
+        return in_memory.getvalue()
 
 # Charge le dernier jeu de données et l'importe dans le modèle
 
@@ -20,7 +30,9 @@ def handle(event, context):
     srcWb = load_workbook_from_url(
         'https://bourse.societegenerale.fr/EmcWebApi/api/ProductSearch/Export?PageNum=1&ProductClassificationId=19&AssetTypeId=2&AssetTypeMenuId=35')
     # srcWb = load_workbook(path.join(sys.path[0], 'export.xlsx'))
-    modWb = load_workbook(path.join(sys.path[0], 'modele.xlsx'))
+    modWb = load_workbook_from_url(
+        'https://raw.githubusercontent.com/qlefevre/bonuscappes/main/functions/modele.xlsx')
+    # modWb = load_workbook(path.join(sys.path[0], 'modele.xlsx'))
     # Onglets Export
     srcWs = srcWb['EXPORT']
     modWs = modWb['EXPORT']
@@ -29,9 +41,19 @@ def handle(event, context):
         for cell in row:
             modWs[cell.coordinate].value = cell.value
 
-    modWb.save(path.join(sys.path[0], 'resultat.xlsx'))
+    # modWb.save(path.join(sys.path[0], 'resultat.xlsx'))
 
-    return 'dsd'
+    output = save_virtual_workbook(modWb)
+
+    return {
+        "body":  base64.b64encode(output).decode('UTF-8'),
+        "statusCode": 200,
+        "isBase64Encoded": True,
+        "headers": {
+            "Content-Type": "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+            "Content-Disposition": "attachment;filename=test.xlsx"
+        }
+    }
 
 
 # main
